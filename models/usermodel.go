e1627fe78b57a8b5c34b0e26deb80a5bbdd62e30
unknown <pigbirthin0320@gmai.com>
2025-07-02 15:04:27 +0800

    修正職責不明確, package引用錯誤

--- models/usermodel.go
+++ models/usermodel.go
@@ -8,7 +8,7 @@ import (
 
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Username  string             `bson:"username" json:"username" binding:"required"`
+	Username  string             `bson:"username" json:"_" binding:"required"`
 	Password  string             `bson:"password" json:"password" binding:"required,min=6"`
 	Age       int                `bson:"age" json:"age" binding:"required,gte=0,lte=150"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
